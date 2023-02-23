0b1e00027d459a6f1e8bf768c8947e128d34d6e2
Mihail_Akmurin <akmurzinmihail@gmail.com>
2023-02-24 01:43:06 +0500

    Пам пам

--- Tinsort/solution1.h
+++ Tinsort/solution1.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-enum Type {Begin,End};
+enum Type {Begin,Point,End};
 
 struct Line {
 	int start;
@@ -22,25 +22,39 @@ bool compare(Event e1,Event e2) {
 }
 
 void solution1() {
-	vector<Line> line = { {0,5},{0,10},{3,12},{1,8} ,{6,15} };
+	vector<Line> line = { {0,5},{2,10},{3,12},{1,8} ,{6,15} };
 	vector<Event> events;
+	vector<int> points = { 1,3,8 };
+	vector<int> results(3,0);
 
 	for (Line l : line) {
 		events.push_back({ l.start,Begin });
 		events.push_back({ l.end,End });
 	}
+
+	for (auto p : points) {
+		events.push_back({ p,Point });
+	}
+
 	sort(events.begin(), events.end(), compare);
-	int n = 0, maxN = 0;
+	int n = 0, maxN = 0, point_num = 0;
 	for (Event e : events) {
-		if (e.type == Begin) {
+		switch (e.type) {
+		case Begin:
 			n++;
 			maxN = max(n, maxN);
-		}
-		else {
+			break;
+		case Point:
+			results[point_num] = n;
+			point_num++;
+			break;
+		case End:
 			n--;
+			break;
 		}
 	}
-	cout << maxN;
+	cout << maxN << endl;
+	for (auto r : results) { cout << r << " "; }
 }
 
 /*0__________0
